309bff74a5b0e27a119b837c9cd023129a8d6d07
carlos784790 <136924728+carlos784790@users.noreply.github.com>
2023-10-14 00:42:32 +0000

    Pending changes exported from your codespace

--- src/app.js
+++ src/app.js
@@ -9,3 +9,34 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+function init() {
+  const button = document.querySelector("#bttn");
+  const excuseElement = document.querySelector("#excuse");
+  if (button && excuseElement) {
+    button.addEventListener("click", () => {
+      excuseElement.innerHTML = generateExcuse();
+    });
+  } else {
+    console.error("Button or excuse element not found!");
+  }
+  console.log("Initialization complete!");
+}
+window.onload = init;
+const generateExcuse = () => {
+  const who = ["The dog", "My grandma", "Bob's turtle", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "the keys", "the car"];
+  const when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  const whoIndex = Math.floor(Math.random() * who.length);
+  const actionIndex = Math.floor(Math.random() * action.length);
+  const whatIndex = Math.floor(Math.random() * what.length);
+  const whenIndex = Math.floor(Math.random() * when.length);
+  return `${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]} ${when[whenIndex]}`;
+};
